c02bb9968b5873a30249dcf7e195d37c8b4ccc2e
Navodadasun <navaadasun@gmail.com>
2025-12-19 23:49:01 +0530

    ..

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,10 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/Portfolio/', // GitHub Pages base URL
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 })
-
